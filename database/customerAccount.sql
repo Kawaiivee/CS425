badc889f041e3d4b4b6e95d81807009f10be9280
kuerina <hectorhernandez.trivino@gmail.com>
2018-04-28 12:11:47 -0500

    adding a view

--- database/customerAccount.sql
+++ database/customerAccount.sql
@@ -1,12 +1,12 @@
 USE ecommerce;
-
-DROP VIEW `customerAccount`;
-CREATE VIEW `customerAccount` as 
-SELECT Customer.address as address,
-	Customer.customer_name as firstname,
-    Customer.customer_id as id,
-    Customer.email as email,
-    Customer.password as psw,
-    "My Account" as title,
-    "0" as stat
- from Customer
+CREATE OR REPLACE VIEW customerAccount AS
+    SELECT 
+        Customer.address AS address,
+        Customer.customer_name AS firstname,
+        Customer.customer_id AS id,
+        Customer.email AS email,
+        Customer.password AS psw,
+        'My Account' AS title,
+        '0' AS stat
+    FROM
+        Customer
